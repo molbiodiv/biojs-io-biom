bafea76d22aacbb5845e82eb4e14451c309264a4
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-11 10:21:30 +0200

    Init biojs project with slush

---
+++ test/test_biom.js
@@ -0,0 +1,33 @@
+/*
+ * biojs-io-biom
+ * https://github.com/iimog/biojs-io-biom
+ *
+ * Copyright (c) 2015 Markus J. Ankenbrand
+ * Licensed under the MIT license.
+ */
+
+// chai is an assertion library
+var chai = require('chai');
+
+// @see http://chaijs.com/api/assert/
+var assert = chai.assert;
+
+// register alternative styles
+// @see http://chaijs.com/api/bdd/
+chai.expect();
+chai.should();
+
+// requires your main app (specified in index.js)
+var biom = require('../');
+
+describe('biojs-io-biom module', function(){
+  describe('#hello()', function(){
+    it('should return a hello', function(){
+
+      assert.equal(biom.hello('biojs'), ("hello biojs"));
+      
+      // alternative styles
+      biom.hello('biojs').should.equal("hello biojs");
+    });
+  });
+});
