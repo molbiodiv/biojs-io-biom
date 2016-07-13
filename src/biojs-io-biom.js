5069eddc185493acf5821a06273cd32ee974f8bb
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-13 13:22:28 +0200

    Add generated by getter

--- src/biojs-io-biom.js
+++ src/biojs-io-biom.js
@@ -6,15 +6,28 @@
  * Licensed under the MIT license.
  */
 
+export const VERSION = "0.1.0";
+
 /**
  @class Biom
  */
 export class Biom {
-    constructor(){
-        this._id = null;
+    constructor({
+        id: _id = null,
+        format: _format = "Biological Observation Matrix 1.0.0",
+        format_url: _format_url = "http://biom-format.org",
+        type: _type = "OTU table",
+        generated_by: _generated_by = `biojs-io-biom v${VERSION}`
+    } = {}){
+        this._id = _id;
+        this._generated_by = _generated_by;
     }
 
     get id(){
         return this._id;
+    }
+
+    get generated_by(){
+        return this._generated_by;
     }
 }
