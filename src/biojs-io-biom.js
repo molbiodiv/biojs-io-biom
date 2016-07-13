b4a1292e3f988b8cd9724caa196bf5a61202bf55
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-13 11:56:31 +0200

    Add basic documentation

--- src/biojs-io-biom.js
+++ src/biojs-io-biom.js
@@ -1,3 +1,14 @@
+/*
+ * biojs-io-biom
+ * https://github.com/iimog/biojs-io-biom
+ *
+ * Copyright (c) 2016 Markus J. Ankenbrand
+ * Licensed under the MIT license.
+ */
+
+/**
+ @class Biom
+ */
 export class Biom {
 
 }
