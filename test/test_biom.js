88a8b8a706c7a9cf9b62f0c56a09417f47dcb423
Markus Ankenbrand <markus@ankenbrand.me>
2016-07-13 14:09:49 +0200

    Update year in copyright notice

--- test/test_biom.js
+++ test/test_biom.js
@@ -2,7 +2,7 @@
  * biojs-io-biom
  * https://github.com/iimog/biojs-io-biom
  *
- * Copyright (c) 2015 Markus J. Ankenbrand
+ * Copyright (c) 2016 Markus J. Ankenbrand
  * Licensed under the MIT license.
  */
 
