bdad11f9141c9014d086f27cdcc3ddc737a104d3
Markus Ankenbrand <markus@ankenbrand.me>
2016-09-06 15:34:34 +0200

    Add test for getDataMatrix

--- test/test_biom.js
+++ test/test_biom.js
@@ -894,6 +894,19 @@ describe('biojs-io-biom module', () => {
     });
   });
 
+  describe('getDataMatrix should return data in dense format', () => {
+    let rows = [{id: 'r1'},{id: 'r2'},{id: 'r3'},{id: 'r4'},{id: 'r5'}];
+    let cols = [{id: 'c1'},{id: 'c2'},{id: 'c3'},{id: 'c4'},{id: 'c5'}];
+    it('should return correct value for sparse data', () => {
+      let biom = new Biom({rows: rows, columns: cols, matrix_type: 'sparse', data: [[0,1,11],[1,2,13],[4,4,9]]});
+      assert.deepEqual(biom.getDataMatrix(), [[0,11,0,0,0],[0,0,13,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,9]]);
+    });
+    it('should return correct value for dense data', () => {
+      let biom = new Biom({rows: rows, columns: cols, matrix_type: 'dense', data: [[0,1,11,1,0],[0,1,2,0,13],[0,1,0,1,1],[1,20,0,0,13],[3,0,4,4,9]]});
+      assert.deepEqual(biom.getDataMatrix(), [[0,1,11,1,0],[0,1,2,0,13],[0,1,0,1,1],[1,20,0,0,13],[3,0,4,4,9]]);
+    });
+  });
+
   describe('_indexByID should return the index by given id in rows or columns', () => {
     let rows = [{id: 'r1'},{id: 'r2'},{id: 'r3'},{id: 'r4'},{id: 'r5'}];
     let cols = [{id: 'c1'},{id: 'c2'},{id: 'c3'},{id: 'c4'},{id: 'c5'}];
