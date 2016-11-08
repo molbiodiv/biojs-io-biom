55c3c64915e6436c13b97f4adabd4a42e6955b25
Markus J. Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2016-11-08 10:05:22 +0100

    Set version number to 1.0.4

--- src/biojs-io-biom.js
+++ src/biojs-io-biom.js
@@ -12,7 +12,7 @@ const _ = require('lodash');
  * Version
  * @type {string} version of this module
  */
-export const VERSION = '1.0.3';
+export const VERSION = '1.0.4';
 
 /**
  * Default Biom Object for empty initialization
