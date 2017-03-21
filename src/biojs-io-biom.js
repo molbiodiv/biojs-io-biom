020075346f274855d6e29b7c209aec96f6c741e9
Markus Ankenbrand <markus@ankenbrand.me>
2017-03-21 16:50:36 +0100

    Bump version 1.0.7

--- src/biojs-io-biom.js
+++ src/biojs-io-biom.js
@@ -12,7 +12,7 @@ const _ = require('lodash');
  * Version
  * @type {string} version of this module
  */
-export const VERSION = '1.0.6';
+export const VERSION = '1.0.7';
 
 /**
  * Default Biom Object for empty initialization
