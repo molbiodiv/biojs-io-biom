6d75c782bf23100134c54d86ee0b7e7e780e6a33
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2017-07-28 10:30:14 +0200

    Fix test assertions

--- test/test_biom.js
+++ test/test_biom.js
@@ -1639,8 +1639,8 @@ describe('biojs-io-biom module', () => {
             let biom = new Biom({rows: rows, columns: cols, matrix_type: 'sparse', data: sparseData});
             biom.transpose();
             assert.deepEqual(biom.getDataMatrix(), transposedMatrix);
-            assert.equal(biom.rows[2].id === 'c3');
-            assert.equal(biom.columns[0].id === 'r1');
+            assert.equal(biom.rows[2].id, 'c3');
+            assert.equal(biom.columns[0].id, 'r1');
         });
         it('should transpose in dense format', () => {
             let biom = new Biom({rows: rows, columns: cols, matrix_type: 'dense', data: denseData});
